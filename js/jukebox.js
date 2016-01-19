877d952ca33fadfd39bca9b3b99ce2343989ed40
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2016-01-19 15:04:47 -0500

    Got Bones

---
+++ js/jukebox.js
@@ -0,0 +1,26 @@
+var Jukebox = {
+	playlist: []
+	play: function (file) {
+		
+	}
+	stop: function () {
+		
+	}
+	// pause: function () {}
+	load: function (file) {
+		this.playlist.push(file)
+	}
+	queue: function (file) {
+		
+	}
+	// random: function(){}
+}
+
+
+
+
+
+
+$(document).ready(function(){
+
+})
