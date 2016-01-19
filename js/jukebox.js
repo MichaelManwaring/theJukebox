dc9724527410fc0494e49b27a55c8147985f2c9e
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2016-01-19 15:40:25 -0500

    adding stuff

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,24 +1,23 @@
 var Jukebox = {
-	playlist: []
+	playlist: [],
+	show_info: function (file) {
+		
+	},
 	play: function (file) {
 		
-	}
+	},
 	stop: function () {
 		
-	}
+	},
 	// pause: function () {}
 	load: function (file) {
 		this.playlist.push(file)
-	}
+	},
 	queue: function (file) {
 		
-	}
+	},
 	// random: function(){}
 }
-
-
-
-
 
 
 $(document).ready(function(){
