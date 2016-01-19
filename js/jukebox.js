c8a92dcc2972babd794c39daa14c305c797a92bf
Emily McCracken <emilymcc803@gmail.com>
2016-01-19 15:38:50 -0500

    testing buttons and js methods

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,18 +1,19 @@
 var Jukebox = {
-	playlist: []
-	play: function (file) {
-		
-	}
+	playlist: [],
+	play: function(){
+		console.log("play")
+	},
 	stop: function () {
-		
-	}
+		console.log("stop")
+	},
 	// pause: function () {}
 	load: function (file) {
-		this.playlist.push(file)
-	}
+		// this.playlist.push(file)
+		console.log("load")
+	},
 	queue: function (file) {
 		
-	}
+	},
 	// random: function(){}
 }
 
@@ -23,4 +24,19 @@ var Jukebox = {
 
 $(document).ready(function(){
 
-})
+$("#play_button").click(function(){
+	Jukebox.play()
+	console.log('click')
+});
+
+$("#stop_button").click(function(){
+	Jukebox.stop()
+	console.log('click')
+});
+
+$("#upload_button").click(function(){
+	Jukebox.load()
+	console.log('click')
+});
+
+});
